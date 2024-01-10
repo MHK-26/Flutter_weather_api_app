2d0ecd530eaf59d611e8b39ae07b7278ec2b518f
Mohammad Hisham <mzoom26@hotmail.com>
2024-01-10 19:22:42 +0400

    refaactoring code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:weather_app/utils/weather.dart';
 
@@ -42,10 +41,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final f = new DateFormat('dd MMM, yyy HH:mm');
+    final f = DateFormat('dd MMM, yyy HH:mm');
     return Scaffold(
       body: Container(
-        constraints: BoxConstraints.expand(),
+        constraints: const BoxConstraints.expand(),
         decoration: BoxDecoration(
             image: DecorationImage(image: backgroundImg!, fit: BoxFit.cover)),
         child: Column(
@@ -55,7 +54,7 @@ class _HomeScreenState extends State<HomeScreen> {
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
-                  SizedBox(
+                  const SizedBox(
                     height: 40,
                   ),
                   Row(
@@ -64,18 +63,17 @@ class _HomeScreenState extends State<HomeScreen> {
                       Container(
                         child: weatherDisplayIcon,
                       ),
-                      Container(
-                        child: Text(
-                          f.format(DateTime.now().toLocal()).toString(),
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
+                      Text(
+                        f.format(DateTime.now().toLocal()).toString(),
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
                       ),
                     ],
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 15,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 15,
                   ),
                 ],
@@ -89,17 +87,20 @@ class _HomeScreenState extends State<HomeScreen> {
                     children: [
                       Text(
                         '$temp°',
-                        style: TextStyle(color: Colors.white, fontSize: 100),
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 100),
                       ),
                       Column(
                         children: [
                           Text(
                             '$mainWeather',
-                            style: TextStyle(color: Colors.white, fontSize: 50),
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 50),
                           ),
                           Text(
                             '$descWeather',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 20),
                           ),
                         ],
                       ),
@@ -107,7 +108,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   Text(
                     '$timeZone',
-                    style: TextStyle(color: Colors.white, fontSize: 30),
+                    style: const TextStyle(color: Colors.white, fontSize: 30),
                   ),
                 ],
               ),
